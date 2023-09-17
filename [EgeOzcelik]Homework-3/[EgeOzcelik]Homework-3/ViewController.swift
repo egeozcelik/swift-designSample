628c0090f943a7f6239b4f6ab7d0514912d2b388
egeozcelik <e4eozcelik@gmail.com>
2023-09-17 15:54:20 +0300

    first commit

---
+++ [EgeOzcelik]Homework-3/[EgeOzcelik]Homework-3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  [EgeOzcelik]Homework-3
+//
+//  Created by Ege Özçelik on 17.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+     
+}
+
